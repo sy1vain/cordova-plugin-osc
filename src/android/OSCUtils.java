1b6a5e8c33b152c5dc340f58c542332330b2668b
sy1vain <info@sylvain.nl>
2013-07-31 10:58:11 +0200

    adding osc listening part

--- src/android/OSCUtils.java
+++ src/android/OSCUtils.java
@@ -8,7 +8,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.illposed.osc.*;
+
 public class OSCUtils extends CordovaPlugin {
+
+	HashMap<Integer, OSCPortIn> oscIn;
+
 	/**
      * Constructor.
      */
@@ -25,7 +30,35 @@ public class OSCUtils extends CordovaPlugin {
      * @return 			True if the action was valid, false otherwise.
      */
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    	System.out.println("exec " + action);
-    	return false;
+    	try{
+    		if(action.equals("startListening")){
+    			startListening(args.getInt(0));
+    		}else{
+    			return false;
+    		}
+    	}catch(Exception e){
+    		callbackContext.error(e.getMessage());
+    	}
+    	//always return true, we only get here through an exception
+    	return true;
+    }
+
+
+    private void startListening(port) throws SocketException{
+		OSCPortIn oscport = getPortIn(port);
+		if(!oscport.isListening()){
+			oscport.startListening();
+		}
+    }
+
+
+    // port utils
+    private OSCPortIn getPortIn(int port) throws SocketException {
+    	OSCPortIn oscport = oscIn.get(port);
+    	if(oscport==null){
+    		oscport = new OSCPortIn(port);
+    		oscIn.put(port, oscport);
+    	}
+    	return oscport;
     }
 }
--- www/OSCListener.js
+++ www/OSCListener.js
@@ -2,8 +2,12 @@ var OSCListener = function(port){
 	this.port = port;
 }
 
-OSCListener.prototype.on = function(message, succes){
-	cordova.exec(succes, function(err){
+OSCListener.prototype.startListening = function(successCallback, errorCallback){
+	cordova.exec(successCallback, errorCallback, "OSC", "startListening", [this.port]);
+}
+
+OSCListener.prototype.on = function(message, successCallback){
+	cordova.exec(successCallback, function(err){
 		console.log(err);
 	}, "OSC", "addMessageListener", [this.port, message]);
 }
